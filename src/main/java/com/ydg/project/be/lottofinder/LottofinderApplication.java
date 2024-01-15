017f88476b0bbaee72e37ac0f3eed26051220d3d
DEREVGEN <96948245+DEREVGEN@users.noreply.github.com>
2024-01-15 23:37:07 +0900

    초기 프로젝트 세팅

---
+++ src/main/java/com/ydg/project/be/lottofinder/LottofinderApplication.java
@@ -0,0 +1,13 @@
+package com.ydg.project.be.lottofinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottofinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottofinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ydg/project/be/lottofinder/LottofinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydg.project.be.lottofinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottofinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
