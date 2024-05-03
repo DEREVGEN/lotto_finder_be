66906ff61c63f93462091e6c167dd7762b0d97ba
DEREVGEN <96948245+DEREVGEN@users.noreply.github.com>
2024-05-03 13:31:32 +0900

    refactor & feat: test코드 통합 테스트로 수정 - api v2에 대한 테스트 코드 추가

--- src/test/java/com/ydg/project/be/lottofinder/controller/LottoResultControllerTest.java
+++ src/test/java/com/ydg/project/be/lottofinder/controller/LottoResultControllerTest.java
@@ -1,38 +1,43 @@
 package com.ydg.project.be.lottofinder.controller;
 
-import com.ydg.project.be.lottofinder.dto.LottoResultResDto;
-import com.ydg.project.be.lottofinder.service.LottoResultService;
+import com.ydg.project.be.lottofinder.entity.LottoResultEntity;
+import com.ydg.project.be.lottofinder.repository.LottoResultRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 
-import static org.mockito.Mockito.when;
-
 @SpringBootTest
 @AutoConfigureWebTestClient
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class LottoResultControllerTest {
 
     @Autowired
     WebTestClient webTestClient;
 
-    @MockBean
-    LottoResultService resultService;
+    @Autowired
+    LottoResultRepository lottoResultRepository;
+
+    @BeforeAll
+    @DisplayName("DB에 데이터 세팅")
+    public void setData() {
+        for (int round = 1100; round >= 1094; round--) {
+            LottoResultEntity lottoResult = new LottoResultEntity(round, "1,2,3,4,5,6,7", 1000000L, LocalDate.now());
+
+            lottoResultRepository.save(lottoResult).block();
+        }
+    }
+
 
     @Test
     @DisplayName("해당 round에 대한 로또 결과 response 테스트")
-    public void checkLottoReulstResponseWithRound() throws Exception {
-        LottoResultResDto resultResDto = setLottoRulestDto();
-
-        when(resultService.getLottoResult(1100)).thenReturn(Mono.just(resultResDto));
+    public void checkLottoReulstResponseWithRound() {
 
         webTestClient.get()
                 .uri("/lotto/result/{round}", 1100)
@@ -45,10 +50,6 @@ class LottoResultControllerTest {
     @Test
     @DisplayName("해당 round에 대한 다수의 로또 결과 response 테스트")
     public void checkLottoResultsResponseWithRound() {
-        LottoResultResDto resultResDto = setLottoRulestDto();
-
-        when(resultService.getLottoResults(1100)).thenReturn(Mono.just(resultResDto));
-
         webTestClient.get()
                 .uri("/lotto/result/{round}/multiple", 1100)
                 .exchange()
@@ -61,26 +62,24 @@ class LottoResultControllerTest {
                 .jsonPath("$.lastLottoResults[3].round").isEqualTo(1096)
                 .jsonPath("$.lastLottoResults[4].round").isEqualTo(1095)
                 .jsonPath("$.lastLottoResults[5].round").isEqualTo(1094);
-
     }
 
-    public LottoResultResDto setLottoRulestDto() {
-        LottoResultResDto resultResDto = new LottoResultResDto();
-        resultResDto.setRound(1100);
-        resultResDto.setLottoNumbers("1,2,3,4,5,6,7");
-        resultResDto.setWinPrize(1000000);
-        resultResDto.setDate(LocalDate.now());
+    @Test
+    @DisplayName("해당 round에 대한 다수의 로또 결과 response 테스트 - v2")
+    public void checkLottoResultsResponseWithRoundV2() {
 
-        for (int round = 1099; round >= 1094; round--) {
-            LottoResultResDto resultResDtoElement = new LottoResultResDto();
-            resultResDtoElement.setRound(round);
-            resultResDtoElement.setLottoNumbers("1,2,3,4,5,6,7");
-            resultResDtoElement.setWinPrize(1000000);
-            resultResDtoElement.setDate(LocalDate.now());
-
-            resultResDto.addLottoResult(resultResDtoElement);
-        }
-        return resultResDto;
+        webTestClient.get()
+                .uri("/lotto/result/v2/{round}/multiple", 1100)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$[0].round").isEqualTo(1100)
+                .jsonPath("$[1].round").isEqualTo(1099)
+                .jsonPath("$[2].round").isEqualTo(1098)
+                .jsonPath("$[3].round").isEqualTo(1097)
+                .jsonPath("$[4].round").isEqualTo(1096)
+                .jsonPath("$[5].round").isEqualTo(1095)
+                .jsonPath("$[6].round").isEqualTo(1094);
     }
 
 }
--- src/test/java/com/ydg/project/be/lottofinder/controller/LottoStoreControllerTest.java
+++ src/test/java/com/ydg/project/be/lottofinder/controller/LottoStoreControllerTest.java
@@ -1,13 +1,20 @@
 package com.ydg.project.be.lottofinder.controller;
 
 import com.ydg.project.be.lottofinder.dto.WinStoreResDto;
+import com.ydg.project.be.lottofinder.entity.LottoStoreEntity;
+import com.ydg.project.be.lottofinder.entity.WinStoreEntity;
+import com.ydg.project.be.lottofinder.repository.LottoStoreRepository;
+import com.ydg.project.be.lottofinder.repository.WinStoreRepository;
 import com.ydg.project.be.lottofinder.service.LottoStoreService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
@@ -23,13 +30,36 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureWebTestClient
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class LottoStoreControllerTest {
 
     @Autowired
     WebTestClient webTestClient;
 
-    @MockBean
-    LottoStoreService lottoStoreService;
+    @Autowired
+    WinStoreRepository winStoreRepository;
+
+    @Autowired
+    LottoStoreRepository lottoStoreRepository;
+
+    @BeforeAll
+    @DisplayName("DB에 데이터 세팅")
+    public void setData() {
+        for (int i = 0; i < 3; i++) {
+
+            LottoStoreEntity lottoStore = LottoStoreEntity.builder()
+                    .storeFid(i)
+                    .storeName("good")
+                    .location(new GeoJsonPoint(90, 126.6705))
+                    .build();
+
+            lottoStoreRepository.save(lottoStore).block();
+
+            WinStoreEntity winStore = new WinStoreEntity(true, 1000, i);
+
+            winStoreRepository.save(winStore).block();
+        }
+    }
 
     @Test
     @DisplayName("사용자 인근 위치 컨트롤러 요청 에러 테스트")
@@ -47,26 +77,12 @@ class LottoStoreControllerTest {
     @Test
     @DisplayName("특정 라운드의 당첨가게 조회 테스트")
     public void checkWinStoreWithRound() {
-
-
-        List<WinStoreResDto> winStoreResDtoList = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            WinStoreResDto winStoreResDto = new WinStoreResDto();
-            winStoreResDto.setRound(1000);
-            winStoreResDto.setAuto(true);
-            winStoreResDtoList.add(winStoreResDto);
-        }
-
-        when(lottoStoreService.getWinLottoStore(eq(1000)))
-                .thenReturn(Flux.fromIterable(winStoreResDtoList));
-
         webTestClient
                 .get()
                 .uri("/lotto/store/win/1000")
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody()
-                .jsonPath("$[*].round").isArray()
                 .jsonPath("$[*].round").value(hasItem(1000));
     }
 }
